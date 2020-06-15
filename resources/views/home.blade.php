614ca57c6b8ba14d6c67c845183c6a166790f23d
Guna Saputra <gusnarichman@gmail.com>
2020-06-16 02:11:57 +0800

    last commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Sublime project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>MakeUpBeauty</title>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+<link href="{{asset('assets/css/mdb.min.css')}}" rel="stylesheet">
+<link rel="shortcut icon" href="{{asset('assets/User/images/shirt.png')}}" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/User/styles/bootstrap4/bootstrap.min.css')}}">
+<link href="{{ asset('assets/User/plugins/font-awesome-4.7.0/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/User/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/User/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/User/plugins/OwlCarousel2-2.2.1/animate.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/User/styles/main_styles.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/User/styles/responsive.css')}}">	
+</head>
+<body>
+<div class="super_container">
+
+	<!-- Header -->
+
+	@extends('layouts.navbar')
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_slider_container">
+			
+			<!-- Home Slider -->
+			<div class="owl-carousel owl-theme home_slider">
+				
+				<!-- Slider Item -->
+				<div class="owl-item home_slider_item">
+					<div class="home_slider_background" style="background-image:url(assets/User/images/home_slider_2.jpg)"></div>
+					<div class="home_slider_content_container">
+						<div class="container">
+							<div class="row">
+								<div class="col">
+									<div class="home_slider_content"  data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+										<div class="home_slider_title" style="color:black; background-color: rgba(255, 255, 255, 0.5)">MakeUpBeauty</div>
+										<div class="home_slider_subtitle" style="color:black; background-color: rgba(255, 255, 255, 0.5)">Rasakan sensasi berbelanja menyenangkan hanya di MakeUpBeauty</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Slider Item -->
+				<div class="owl-item home_slider_item">
+					<div class="home_slider_background" style="background-image:url(assets/User/images/home_slider_3.jpg)"></div>
+					<div class="home_slider_content_container">
+						<div class="container">
+							<div class="row">
+								<div class="col">
+									<div class="home_slider_content"  data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+										<div class="home_slider_title">Buat Doi Klepek Klepek</div>
+										<div class="home_slider_subtitle">Kamu siapa ya?</div>
+										<div class="button button_light home_button"><a href="#">Shop Now</a></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Slider Item -->
+				<div class="owl-item home_slider_item">
+					<div class="home_slider_background" style="background-image:url(assets/User/images/home_slider_1.jpg)"></div>
+					<div class="home_slider_content_container">
+						<div class="container">
+							<div class="row">
+								<div class="col">
+									<div class="home_slider_content"  data-animation-in="fadeIn" data-animation-out="animate-out fadeOut">
+										<div class="home_slider_title">#RIBETLESS</div>
+										<div class="home_slider_subtitle">Dapatkan segera produk kecantikan yang Anda inginkan</div>
+										<div class="button button_light home_button"><a href="#">Shop Now</a></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<!-- Home Slider Dots -->
+			
+			<div class="home_slider_dots_container">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="home_slider_dots">
+								<ul id="home_slider_custom_dots" class="home_slider_custom_dots">
+									<li class="home_slider_custom_dot active">01.</li>
+									<li class="home_slider_custom_dot">02.</li>
+									<li class="home_slider_custom_dot">03.</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+
+		</div>
+	</div>
+
+	<!-- Ads -->
+
+	<div class="avds">
+		<div class="avds_container d-flex flex-lg-row flex-column align-items-start justify-content-between">
+			<div class="avds_small">
+				<div class="avds_background" style="background-image:url(assets/User/images/avds_small.jpg)"></div>
+				<div class="avds_small_inner">
+					<div class="avds_discount_container">
+						<img src="{{ asset('assets/User/images/discount.png')}}" alt="">
+						<div>
+							<div class="avds_discount">
+								<div>20<span>%</span></div>
+								<div>Discount</div>
+							</div>
+						</div>
+					</div>
+					<div class="avds_small_content">
+						<div class="avds_title" style="background-color: rgba(0, 0, 0, 0.5)">Brush</div>
+						<div class="avds_text" style="background-color: rgba(0, 0, 0, 0.5)">Jangan lewatkan #passthebrushchallenge</div>
+						<div class="avds_link"><a href="/product/2">See More</a></div>
+					</div>
+				</div>
+			</div>
+			<div class="avds_large">
+				<div class="avds_background" style="background-image:url(assets/User/images/avds_large.jpg)"></div>
+				<div class="avds_large_container">
+					<div class="avds_large_content">
+						<div class="avds_title">MUA Profesional</div>
+						<div class="avds_text">Wujudkan mimpimu untuk ikut serta dalam #LATHICHALLENGE</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  
+	<!-- Products -->
+	<br><br>
+	<hr class="mb-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm d-flex">
+				<div class="form-group">
+					<p style="font-size: 50px"><i>CATEGORIES</i></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr class="mb-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm d-flex">
+				<div class="form-group">
+					<input class="form-check-input radiobtn" name="group100" type="radio" id="radio100" selected checked value="0">
+					<label for="radio100" class="form-check-label dark-grey-text">All</label>
+				</div>
+			</div>
+			  <input id="signup-token" name="_token" type="hidden" value="{{csrf_token()}}">
+		  @foreach ($category as $item)
+			  @if ($item->product->count())
+				  <div class="col-sm d-flex">
+					<div class="form-group">
+						  <input class="form-check-input radiobtn" name="group100" type="radio" id="radio10{{$loop->iteration}}" value="{{$item->id}}">
+						  <label for="radio10{{$loop->iteration}}" class="form-check-label dark-grey-text">{{$item->category_name}}</label>
+					  </div>
+				  </div>
+				  <br>
+			  @else
+				  <input type="hidden" id="radio10{{$loop->iteration}}" class="radiobtn">
+			  @endif
+		  @endforeach
+		</div>
+	</div>
+	<hr class="mb-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm d-flex">
+				<div class="form-group">
+					<p style="font-size: 50px"><i>PRODUCTS</i></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<hr class="mb-5">
+	<div class="ganti">
+	<div class="products">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="product_grid">
+						@foreach ($product as $products)
+						<!-- Product -->
+						<div class="product">
+							@foreach ($products->product_image as $image)
+							<div class="product_image"><img style="width:250px;height:250px;" src="/uploads/product_images/{{$image->image_name}}" alt=""></div>
+								@break
+							@endforeach
+							@php
+								$home = new Home;
+                            	$disc = $home->tampildiskon($products->discount);
+							@endphp
+							@if($disc!=0)
+								<div style="background-color:red;"class="product_extra product_new"><a href="categories.html">-{{$disc}}%</a></div>
+							@endif
+								<div class="product_content">
+									<div class="product_title"><a href="/product/{{$products->id}}">{{$products->product_name}}</a></div>
+									<span class="badge badge-primary mb-2">Rating: {{$products->product_rate}} <i class="fa fa-star"></i></span>
+									@if ($products->stock == 0)
+									<span class="badge badge-danger mb-2">Out Of Stock!</span>
+									@endif	
+										@php
+											$home = new Home;
+                            				$harga = $home->diskon($products->discount,$products->price);
+										@endphp
+										@if ($harga != 0)	   
+											<div style="text-decoration:line-through;" class="product_price">Rp. {{number_format($products->price)}}</div>
+											<div style="font-weight:bold;color:black;" class="product_price">Rp. {{number_format($harga)}}</div>
+										@else
+											<div class="product_price">Rp. {{number_format($products->price)}}</div>
+										@endif
+								</div>
+						</div>
+						@endforeach
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	<hr class="mb-5">
+
+	<!-- Ad -->
+	<div class="avds_xl">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="avds_xl_container clearfix">
+						<div class="avds_xl_background" style="background-image:url(assets/User/images/avds_xl.jpg)"></div>
+						<div class="avds_xl_content">
+							<div class="avds_title">Cegah Penyebaran COVID-19</div>
+							<div class="avds_text">#BELANJADARIRUMAH</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Icon Boxes -->
+
+	<div class="icon_boxes">
+		<div class="container">
+			<div class="row icon_box_row">
+				
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box">
+						<div class="icon_box_image"><img src="assets/User/images/icon_1.svg" alt=""></div>
+						<div class="icon_box_title">Shipping to All Around Indonesia</div>
+						<div class="icon_box_text">
+							<p>Medukung ekspedisi yang menjangkau keseluruh Indonesia</p>	
+						</div>
+					</div>
+				</div>
+
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box">
+						<div class="icon_box_image"><img src="assets/User/images/icon_2.svg" alt=""></div>
+						<div class="icon_box_title">Free Returns</div>
+						<div class="icon_box_text">
+							<p>Produk tidak sesuai? tidak original? KEMBALIKAN! GRATIS!</p>
+						</div>
+					</div>
+				</div>
+
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box">
+						<div class="icon_box_image"><img src="assets/User/images/icon_3.svg" alt=""></div>
+						<div class="icon_box_title">24h Fast Support</div>
+						<div class="icon_box_text">
+							<p>Cek SosMed kami melalui link yang tersedia</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Newsletter -->
+
+	<div class="newsletter">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="newsletter_border"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<footer>
+<script src="{{ asset('assets/User/js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{ asset('assets/User/styles/bootstrap4/popper.js')}}"></script>
+<script src="{{ asset('assets/User/styles/bootstrap4/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/User/plugins/greensock/TweenMax.min.js')  }}"></script>
+<script src="{{ asset('assets/User/plugins/greensock/TimelineMax.min.js')  }}"></script>
+<script src="{{ asset('assets/User/plugins/scrollmagic/ScrollMagic.min.js')  }}"></script>
+<script src="{{ asset('assets/User/plugins/greensock/animation.gsap.min.js')  }}"></script>
+<script src="{{ asset('assets/User/plugins/greensock/ScrollToPlugin.min.js')  }}"></script>
+<script src="{{ asset('assets/User/plugins/OwlCarousel2-2.2.1/owl.carousel.js')  }}"></script>
+<script src="{{ asset('assets/User/plugins/Isotope/isotope.pkgd.min.js')  }}"></script>
+<script src="{{ asset('assets/User/plugins/easing/easing.js')  }}"></script>
+<script src="{{ asset('assets/User/plugins/parallax-js-master/parallax.min.js')  }}"></script>
+<script src="{{ asset('assets/User/js/custom.js')  }}"></script>
+</footer>
+<script>
+    jQuery(document).ready(function(e){
+        jQuery('.radiobtn').click(function(e){
+            var index = $('.radiobtn').index(this);
+            console.log(jQuery('#radio10'+index).val());
+            jQuery.ajax({
+                url: "{{url('/show_categori')}}",
+                method: 'post',
+                data: {
+                    _token: $('#signup-token').val(),
+                    id: jQuery('#radio10'+index).val(),
+                },
+                success: function(result){
+                    $('.ganti').html(result.hasil);
+                }
+            });
+        });
+
+        jQuery('#search').keyup(function(e){
+          var isi = $('#search').val();
+          jQuery.ajax({
+                url: "{{url('/show_categori')}}",
+                method: 'post',
+                data: {
+                    _token: $('#signup-token').val(),
+                    id: -1,
+                    cari: isi 
+                },
+                success: function(result){
+                    $('.ganti').html(result.hasil);
+                }
+            });
+        });
+    });
+	
+</script>
+
+</body>
+</html>
---
+++ resources/views/product/admincreate.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.product')
+@section('content')
+
+<style>
+	.thumbnail{
+		display: inline-block;
+		height: 100px;
+		margin: 10px;    
+	}
+</style>
+
+<div class="container">
+	<div class="row align-items-centre">
+		<div class="col-lg-2">
+		</div>
+		<div class="col-md-8">
+			<div class="component">
+				<div class="card">
+					<form class="form-signin" action="{{ route('products.store') }}" method="post" enctype="multipart/form-data">
+						@csrf
+					<div class="card-header">
+						<center>
+							<h2>Tambah Produk</h2>
+							<br>
+						</center>
+					</div>
+				<div class="card-body">
+					<div class="form-group">
+						<label>Nama Produk</label>
+						<input type="text" class="form-control" placeholder="Nama Produk" aria-label="Nama Produk" aria-describedby="basic-addon1" name="product_name">
+					</div>
+					<div class="form-group">
+						<label>Harga</label>
+						<input type="text" class="form-control" placeholder="Harga" aria-label="Harga Satuan" aria-describedby="basic-addon1" name="price">
+					</div>
+					<div class="form-group">
+						<label>Stock</label>
+						<input type="text" class="form-control" placeholder="Stock" aria-label="Stock" aria-describedby="basic-addon1" name="stock">
+					</div>
+					<div class="form-group">
+						<label>Berat Produk</label>
+						<input type="text" class="form-control" placeholder="Berat Produk" aria-label="Berat Produk" aria-describedby="basic-addon1" name="weight">
+					</div>
+					<div class="form-group">
+						<label class="control-label">Kategori</label>
+						<select name="category_id[]" class="selectpicker form-control" multiple data-live-search="true"  multiple="multiple" required>
+							@foreach ($categories as $category)
+								<option value="{{ $category->id }}">{{ $category->category_name }}</option>
+							@endforeach
+						</select>
+						<br>
+					</div>
+					<div class="form-group">
+						<strong>Description</strong>
+						<textarea class="form-control" col="4" name="description" placeholder="Deskripsi Produk"></textarea>
+					</div>
+					<div class="card-body">
+						<div class="form-group">
+							<label>Pilih Foto</label>
+							<input type="file" class="form-control" placeholder="Nama Produk" aria-label="Nama Produk" aria-describedby="basic-addon1" name="files[]" multiple required id="files">
+							<output id="result" />
+						</div>	
+					</div>
+						<div class="card-footer" align="center">
+							<button class="btn btn-md btn-primary btn-icon-text" type="submit">
+								<i class="mdi mdi-arrow-right btn-icon-prepend fa fa-plus"></i> Add Product
+							</button>
+						</div>
+					</form>
+					
+				@if (count($errors) > 0)
+				<div class="alert alert-danger">
+					<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+					</ul>
+				</div>
+				@endif
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+        
+		//Check File API support
+		if(window.File && window.FileList && window.FileReader)
+		{
+			var filesInput = document.getElementById("files");
+			
+			filesInput.addEventListener("change", function(event){
+				
+				
+				var files = event.target.files; //FileList object
+				var output = document.getElementById("result");
+				
+				output.innerHTML = "";
+				
+				for(var i = 0; i< files.length; i++)
+				{
+					var file = files[i];
+					
+					//Only pics
+					if(!file.type.match('image'))
+					  continue;
+					
+					var picReader = new FileReader();
+					
+					picReader.addEventListener("load",function(event){
+						
+						var picFile = event.target;
+						
+						var div = document.createElement("span");
+						
+						div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
+								"title='" + picFile.name + "'/>";
+						
+						output.insertBefore(div,null);            
+					
+					});
+					
+					 //Read the image
+					picReader.readAsDataURL(file);
+				}                               
+			   
+			});
+		}
+		else
+		{
+			console.log("Your browser does not support File API");
+		}
+</script>
+
+
+@endsection
